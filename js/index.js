5ee22d52ba3ce76c9a83b30123df799058afb0d9
Micaela <micaelamendoza1998@gmail.com>
2021-11-26 21:01:30 -0300

    desafío 3 v1.2.0

--- js/index.js
+++ js/index.js
@@ -1,28 +1,18 @@
 /**
- * @challenge: Crear un algoritmo con un condicional - Desafío 2
+ * @challenge: Crear un algoritmo utilizando un ciclo - Desafío 3
  * 
- * @version: v1.1.0
+ * @version: v1.2.0
  * @author: Micaela Mendoza
- * @fecha: 25/11/2021
+ * @fecha: 26/11/2021
  *
  * History:
  * v1.1.0 - Desafío 1
+ * v1.1.0 - Desafío 2
  */
 
-let name = prompt('Ingrese su nombre');
-let rooms = parseInt(prompt('Ingrese la cantidad de ambientes que tiene su domicilio'));
-let cost = 0;
+let number = parseInt(prompt('Ingrese un número impar para ingresar a la página'));
 
-if (rooms > 0 && rooms <= 1) {
-	cost = 2000;
-} else if (rooms > 1 && rooms <= 4) {
-	cost = 4000;
-} else if (rooms > 4) {
-	cost = 6000;
-} else {
-	alert('Ingrese un número válido');
-}
-
-if (!isNaN(rooms) && rooms > 0) {
-	alert('Hola ' + name + '! El costo básico de su solicitud es $' + cost);
+while (number % 2 === 0) {
+	alert('Inténtelo nuevamente');
+	number = parseInt(prompt('Ingrese un número impar para ingresar a la página'));
 }
