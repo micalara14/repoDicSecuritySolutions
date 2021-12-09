0b36126a35d95b560b896d8708e26b3bc2d303c5
Micaela <micaelamendoza1998@gmail.com>
2021-12-09 19:47:48 -0300

    Primera entrega del proyecto final

--- js/index.js
+++ js/index.js
@@ -13,6 +13,45 @@
  * v1.4.0 - Desafío 5
  */
 
+//BIENVENIDA A LA PÁGINA
+let name = prompt('Ingrese su nombre');
+
+let roomPrice = parseInt(prompt('Ingrese número de habitaciones a asegurar'));
+
+if (roomPrice <= 2 && roomPrice > 0) {
+	roomPrice = 2000;
+} else if (roomPrice && roomPrice > 0) {
+	roomPrice = 3000;
+} else if (roomPrice >= 4 && roomPrice > 0) {
+	roomPrice = 4000;
+} else {
+	alert('Ingrese un número válido');
+}
+
+let installationPrice = 1000;
+
+if (roomPrice > 0) {
+	function sum(roomPrice, installationPrice) {
+		total = roomPrice + installationPrice;
+		alert('Hola, ' + name + '! El precio del trabajo con instalación incluida arranca en $' + total);
+	}
+} else {
+}
+
+sum(roomPrice, installationPrice);
+
+let plus = prompt('Quiere agregar un seguro? Ingrese SI o NO');
+let plusTotal = total + 1000;
+let wPlusTotal = total;
+
+if (plus.toUpperCase() == 'SI') {
+	alert('El precio final es $' + plusTotal + '. En la consola verá las distintas opciones de cámaras');
+} else if (plus.toUpperCase() == 'NO') {
+	alert('El precio final es $' + wPlusTotal + '. En la consola verá las distintas opciones de cámaras');
+} else {
+	alert('Ingrese una respuesta válida');
+}
+
 //ARRAY UNO
 let cameras = [
 	{ brand: 'Hikvision', model: 1.6, range: '2.5mts', price: 4900 },
@@ -31,20 +70,9 @@ console.log(cameras); //Mostrando array 1 por consola
 console.log(moreCameras); //Mostrando array 2 por consola
 console.log(allCameras); //Mostrando concatenación de arrays por consola
 
-console.log(allCameras.filter((camera) => camera.price < 4500)); //Mostrando por consola cámaras con un precio menor que $4500
-
 //ORDENANDO ARRAY POR PRECIO DE MENOR A MAYOR
 console.log(
 	allCameras.sort(function(a, b) {
 		return a.price - b.price;
 	})
 );
-
-//ARRAYS DE NÚMEROS Y ORDEN
-let numbers = [ 23, 76, 899, 1, 45, 543, 666, 432, 14 ];
-
-numbers.sort(function(a, b) {
-	return a - b;
-});
-
-console.log(numbers);
